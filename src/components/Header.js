7145f79c0effd9319a8987e228e6900bf4ff5e41
GriffithSutcliff <darkborn56@gmail.com>
2024-08-18 22:21:07 +0300

    header-fix

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from '../imgs/logo.png'
+import planet from '../imgs/planet.svg'
+
+const Header = () => {
+    return (
+        <div className='header'>
+           <img src={logo}></img>
+           <input placeholder='Введите артикул товара...'></input>
+           <div className='header-info'>
+            <div>Компания</div>
+            <div>Продукция</div>
+            <div>Поддержка</div>
+            <div>Где купить</div>
+           </div>
+           <button>Вход</button>
+           <div className='lang'>
+            <img src={planet}></img>
+            <select>
+                    <option>RU</option>
+                    <option>EN</option>
+            </select>
+           </div>
+        </div>
+    );
+};
+
+export default Header;
