2d415ea21b81fc929045fec833400d7c3f770205
GriffithSutcliff <darkborn56@gmail.com>
2024-08-19 22:52:34 +0300

    frame2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Frame1 from "./components/Frame1";
+import Frame2 from "./components/Frame2";
 import Header from "./components/Header";
 import Search from "./components/Search";
 
@@ -8,6 +9,9 @@ function App() {
       <Header />
       <Search />
       <Frame1 />
+      <div className="dark-theme">
+        <Frame2 />
+      </div>
     </div>
   );
 }
---
+++ src/components/Frame2.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import pads from '../imgs/pads.png'
+
+const Frame2 = () => {
+    return (
+        <div className='frame-2'>
+            <img className='pads' src={pads}></img>
+            <div className='frame-2-text'>
+                <div className='h'>ТОВАРЫ</div>
+                <div className='context'>ДИСКИ И КОЛОДКИ BRANNOR ЭТО ВЫСОКОТЕХНОЛОГИЧНЫЙ И МАКСИМАЛЬНО ЭФФЕКТИВНЫЙ ПРОДУКТ</div>
+                <button>увеличение плавности торможения без потери эффективности</button>
+                <div className='group'>
+                    <button>увеличенный срок службы</button>
+                    <button>увеличинная эффективность работы</button>
+                </div>
+                <button>стабильные характеристики в широком диапазоне температур</button>
+            </div>
+        </div>
+    );
+};
+
+export default Frame2;
